6995b1bc909134296362f6db62475a4f5f745554
Kris Paisarn <kris.paisarn@gmail.com>
2021-01-20 12:17:37 +0700

    Updates

---
+++ precache-manifest.74a7707b093ff903e48a5cc82b52aa05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ce81c027a462964d7953468f060eb8",
+    "url": "/portflio/index.html"
+  },
+  {
+    "revision": "5f6b6e7fa8806eb30b0a",
+    "url": "/portflio/static/css/2.08d8c495.chunk.css"
+  },
+  {
+    "revision": "787c98d3edf32d7b6462",
+    "url": "/portflio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f6b6e7fa8806eb30b0a",
+    "url": "/portflio/static/js/2.91ed1c65.chunk.js"
+  },
+  {
+    "revision": "b5397ceaa0a636d7b8fba9bd95c775e7",
+    "url": "/portflio/static/js/2.91ed1c65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "787c98d3edf32d7b6462",
+    "url": "/portflio/static/js/main.890e439d.chunk.js"
+  },
+  {
+    "revision": "79e2f19e514ddd02d092",
+    "url": "/portflio/static/js/runtime-main.730e9f2b.js"
+  },
+  {
+    "revision": "9bea5fb4c351d49197a123d9fd8cd0e2",
+    "url": "/portflio/static/media/profile.9bea5fb4.png"
+  }
+]);
