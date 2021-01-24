f1932a9162566bee4c0b4bd060c2633517938f61
Kris Paisarn <kris.paisarn@gmail.com>
2021-01-25 06:43:39 +0700

    Updates

---
+++ precache-manifest.84e26fdcffdbe3fdc3ef00df9fde2967.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad32c31c48b14c776ed620b2cb89c6a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99b16fcc797e4e0b5ef7",
+    "url": "./static/css/2.08d8c495.chunk.css"
+  },
+  {
+    "revision": "d53ba3288478ee7a830c",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "99b16fcc797e4e0b5ef7",
+    "url": "./static/js/2.3fe52925.chunk.js"
+  },
+  {
+    "revision": "b5397ceaa0a636d7b8fba9bd95c775e7",
+    "url": "./static/js/2.3fe52925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53ba3288478ee7a830c",
+    "url": "./static/js/main.bb2f91b2.chunk.js"
+  },
+  {
+    "revision": "bb30c7d0fbfa6286a3a5",
+    "url": "./static/js/runtime-main.4e496788.js"
+  },
+  {
+    "revision": "4923302b22ea9c8597485925b8f1ffe4",
+    "url": "./static/media/profile.4923302b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.903304f23545d52ca16a524faaa04f2e.js"
+  "./precache-manifest.84e26fdcffdbe3fdc3ef00df9fde2967.js"
 );
 
 self.addEventListener('message', (event) => {
